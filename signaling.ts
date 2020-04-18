32b2268ce0cc466bcb805b97f8bc865e76f33ead
山本大 <dai.yamamoto@levelenter.com>
2020-04-18 15:54:41 +0900

    ssl delete

--- signaling.ts
+++ signaling.ts
@@ -1,18 +1,7 @@
-
-import https from "https";
-import fs from "fs";
-
-// 秘密鍵とデジタル証明書ファイルを指定する
-// const options = { 
-//   key : fs.readFileSync('./out/server.key'),
-//   cert: fs.readFileSync('./out/server.crt')
-// };
-
 var port = 9001;
-// var io = require('socket.io').listen(port,options); // https
 var io = require('socket.io').listen(port);
-console.log((new Date()) + " Server is listening on port " + port);
- 
+
+console.log((new Date()) + " Server is listening on port " + port); 
 io.sockets.on('connection', function(socket) {
   socket.on('message', function(message) {
     socket.broadcast.emit('message', message);
